7c4e81379ad41bfa9ba2d45026dad726cfe56dc1
William Tam <email.wtam@gmail.com>
2020-09-29 14:09:42 -0400

    remove a bunch of stuffs

--- scripts/openshiftci-presubmit-all-tests.sh
+++ scripts/openshiftci-presubmit-all-tests.sh
@@ -5,53 +5,28 @@ set -e
 # show commands
 set -x
 
-ARCH=$(uname -m)
-export CI="openshift"
-if [ "${ARCH}" == "s390x" ]; then
-    make configure-installer-tests-cluster-s390x
-elif [ "${ARCH}" == "ppc64le" ]; then
-    make configure-installer-tests-cluster-ppc64le
-else
-    make configure-installer-tests-cluster
-fi
+
 make bin
-mkdir -p $GOPATH/bin
-go get -u github.com/onsi/ginkgo/ginkgo
-export PATH="$PATH:$(pwd):$GOPATH/bin"
+export PATH="$PATH:$(pwd)"
 export ARTIFACTS_DIR="/tmp/artifacts"
 export CUSTOM_HOMEDIR=$ARTIFACTS_DIR
 
-# Copy kubeconfig to temporary kubeconfig file
-# Read and Write permission to temporary kubeconfig file
-TMP_DIR=$(mktemp -d)
-cp $KUBECONFIG $TMP_DIR/kubeconfig
-chmod 640 $TMP_DIR/kubeconfig
-export KUBECONFIG=$TMP_DIR/kubeconfig
 
-# Login as developer
-odo login -u developer -p developer
 
-# Check login user name for debugging purpose
-oc whoami
+
+kam version
 
 if [ "${ARCH}" == "s390x" ]; then
     # Integration tests
-    # make test-integration
 
     # E2e tests
-    #make test-e2e-beta
 elif  [ "${ARCH}" == "ppc64le" ]; then
     # Integration tests
-    # make test-generic
 
     # E2e tests
-    #make test-e2e-beta
 else
     # Integration tests
-    # make test-integration
 
     # E2e tests
-    #make test-e2e-all
 fi
 
-odo logout
