4c1f06effcf1504433fc8bd8a8b51d1845d45ad1
arout <arout@redhat.com>
2020-10-05 15:49:44 +0530

    Creating sealed secrets instance

--- scripts/setup-operators.sh
+++ scripts/setup-operators.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-echo "starting"
+echo "Starting sealed secrets operator installation"
 install_sealed_secrets_operator(){
   oc create -f - <<EOF
   apiVersion: operators.coreos.com/v1alpha1
@@ -8,6 +8,15 @@ install_sealed_secrets_operator(){
   metadata: 
     labels: 
       operators.coreos.com/sealed-secrets-operator-helm.cicd: ""
+    managedFields: 
+      - 
+        apiVersion: operators.coreos.com/v1alpha1
+        manager: olm
+        operation: Update
+      - 
+        apiVersion: operators.coreos.com/v1alpha1
+        manager: catalog
+        operation: Update
     name: sealed-secrets-operator-helm
     namespace: cicd
   spec: 
@@ -33,7 +42,64 @@ do
         sleep 10
     fi
 done
+echo "Completes sealed secrets operator installation"
 
+# create_sealed_secrets_operator_instance(){
+#   oc create -f - <<EOF
+#   apiVersion: bitnami.com/v1alpha1
+#   kind: SealedSecretController
+#   metadata: 
+#     finalizers: 
+#       - uninstall-helm-release
+#     managedFields: 
+#       - 
+#         apiVersion: bitnami.com/v1alpha1
+#         manager: oc
+#         operation: Update
+#       - 
+#         apiVersion: bitnami.com/v1alpha1
+#         manager: helm-operator
+#         operation: Update
+#     name: sealedsecretsontroller
+#     namespace: cicd
+#   spec: 
+#     affinity: {}
+#     controller: 
+#       create: true
+#     crd: 
+#       create: true
+#       keep: true
+#     image: 
+#       pullPolicy: IfNotPresent
+#       repository: "quay.io/bitnami/sealed-secrets-controller@sha256:8e9a37bb2e1a6f3a8bee949e3af0e9dab0d7dca618f1a63048dc541b5d554985"
+#     ingress: 
+#       annotations: {}
+#       enabled: false
+#       hosts: 
+#         - chart-example.local
+#       path: /v1/cert.pem
+#       tls: []
+#     networkPolicy: false
+#     nodeSelector: {}
+#     podAnnotations: {}
+#     podLabels: {}
+#     priorityClassName: ""
+#     rbac: 
+#       create: true
+#       pspEnabled: false
+#     resources: {}
+#     secretName: sealed-secrets-key
+#     securityContext: 
+#       fsGroup: ""
+#       runAsUser: ""
+#     serviceAccount: 
+#       create: true
+#       name: ""
+#     tolerations: []
+# EOF
+# }
+
+echo "Starting openshift pipelines operator installation"
 install_openshift_pipelines_operator() {
   oc create -f - <<EOF
   apiVersion: operators.coreos.com/v1alpha1
@@ -41,6 +107,15 @@ install_openshift_pipelines_operator() {
   metadata: 
     labels: 
       operators.coreos.com/openshift-pipelines-operator-rh.openshift-operators: ""
+    managedFields: 
+      - 
+        apiVersion: operators.coreos.com/v1alpha1
+        manager: olm
+        operation: Update
+      - 
+        apiVersion: operators.coreos.com/v1alpha1
+        manager: catalog
+        operation: Update
     name: openshift-pipelines-operator-rh
     namespace: openshift-operators
   spec: 
@@ -66,7 +141,9 @@ do
         sleep 10
     fi
 done
+echo "Completes openshift pipelines operator installation"
 
+echo "Starting argocd operator installation"
 install_argocd_operator(){
   oc create -f - <<EOF
   apiVersion: operators.coreos.com/v1alpha1
@@ -74,6 +151,15 @@ install_argocd_operator(){
   metadata: 
     labels: 
       operators.coreos.com/argocd-operator.argocd: ""
+    managedFields: 
+      - 
+        apiVersion: operators.coreos.com/v1alpha1
+        manager: olm
+        operation: Update
+      - 
+        apiVersion: operators.coreos.com/v1alpha1
+        manager: catalog
+        operation: Update
     name: argocd-operator
     namespace: argocd
   spec: 
@@ -99,3 +185,4 @@ do
         sleep 10
     fi
 done
+echo "Completes argocd operator installation"
