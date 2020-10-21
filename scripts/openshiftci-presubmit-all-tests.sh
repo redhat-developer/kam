4ab5533dccf3f47bbe161daa09861f604b19ed96
arout <arout@redhat.com>
2020-10-21 15:14:54 +0530

    Create developer login session in CI

--- scripts/openshiftci-presubmit-all-tests.sh
+++ scripts/openshiftci-presubmit-all-tests.sh
@@ -12,4 +12,24 @@ export PATH="$PATH:$(pwd)"
 export ARTIFACTS_DIR="/tmp/artifacts"
 export CUSTOM_HOMEDIR=$ARTIFACTS_DIR
 
+# Copy kubeconfig to temporary kubeconfig file and grant
+# read and Write permission to temporary kubeconfig file
+TMP_DIR=$(mktemp -d)
+cp $KUBECONFIG $TMP_DIR/kubeconfig
+chmod 640 $TMP_DIR/kubeconfig
+export KUBECONFIG=$TMP_DIR/kubeconfig
+
+# Login as developer
+oc login -u developer -p developer
+
+# Check login user name for debugging purpose
+oc whoami
+login_user=`oc whoami`
+if [[ $login_user == *"developer"* ]]; then
+    echo "Login to the cluster as a developer user"
+else
+    echo "Fail to login as a developer user"
+    exit 1
+fi
+
 kam version
