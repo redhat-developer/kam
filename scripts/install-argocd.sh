a136e2ed4f4bed32d6226c3e922222e17b5a2b2a
Dewan Ahmed <dewan.ishtiaque@hotmail.com>
2020-10-24 22:09:30 -0400

    Changing default location for argocd binary

--- scripts/install-argocd.sh
+++ scripts/install-argocd.sh
@@ -9,7 +9,7 @@ PLATFORM=`uname -s | awk '{print tolower($0)}'`
 echo $PLATFORM 
 
 # Fetch the appropriate OS binary for ArgoCD
-curl -sSL -o argocd https://github.com/argoproj/argo-cd/releases/download/$VERSION/argocd-$PLATFORM-amd64
+curl -sSL -o bin/argocd https://github.com/argoproj/argo-cd/releases/download/$VERSION/argocd-$PLATFORM-amd64
 
 # Make the argocd CLI executable
 chmod +x argocd
