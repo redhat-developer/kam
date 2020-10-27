22a4d8f1dfa1a3c2e5a9a6c8cd1d53e72aed7a59
Dewan Ahmed <30604461+dewan-ahmed@users.noreply.github.com>
2020-10-27 01:47:52 -0400

    Adding argocd install script that can be referenced from other E2E tests (#71)
    * Adding argocd install script that can be referenced from other E2E test scripts
    
    * Implementing the review comments from Amit
    
    * Implementing review comments.
    
    * Resolving merge conflicts with update
    
    * Changing default location for argocd binary
    
    * Correcting file permission for binary

---
+++ scripts/install-argocd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Find the latest ArgoCD version
+VERSION=$(curl --silent "https://api.github.com/repos/argoproj/argo-cd/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/')
+echo $VERSION
+
+# Platform check
+PLATFORM=`uname -s | awk '{print tolower($0)}'`
+echo $PLATFORM 
+
+# Fetch the appropriate OS binary for ArgoCD
+curl -sSL -o bin/argocd https://github.com/argoproj/argo-cd/releases/download/$VERSION/argocd-$PLATFORM-amd64
+
+# Make the argocd CLI executable
+chmod +x bin/argocd
