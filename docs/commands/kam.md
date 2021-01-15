fc60735d40c3cb09946d86cfb7d3e4d70b0db80b
Kevin McDermott <bigkevmcd@gmail.com>
2021-01-15 16:08:32 +0000

    Add completion command for various shells.
    This adds `kam completion bash` (and others) setting up automated completion
    when using the following shells:
    
     * bash
     * zsh
     * Powershell
     * fish

--- docs/commands/kam.md
+++ docs/commands/kam.md
@@ -16,6 +16,7 @@ GitOps Application Manager (KAM) is a CLI tool to scaffold your GitOps repositor
 
 * [kam bootstrap](kam_bootstrap.md)	 - Bootstrap GitOps CI/CD with a starter configuration
 * [kam build](kam_build.md)	 - Build pipelines files
+* [kam completion](kam_completion.md)	 - Generates shell completion script.
 * [kam environment](kam_environment.md)	 - Manage an environment in GitOps
 * [kam service](kam_service.md)	 - Manage services in an environment
 * [kam version](kam_version.md)	 - Print the version information
---
+++ pkg/cmd/completion.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var completionCmd = &cobra.Command{
+	Use:   "completion [bash|zsh|fish|powershell]",
+	Short: "Generates shell completion script.",
+	Long: `Generates shell completion script for your shell environment
+
+Example:
+
+	source <(kam completion bash)
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			cmd.Help()
+			os.Exit(0)
+		}
+		switch args[0] {
+		case "bash":
+			cmd.Root().GenBashCompletion(os.Stdout)
+		case "zsh":
+			cmd.Root().GenZshCompletion(os.Stdout)
+		case "fish":
+			cmd.Root().GenFishCompletion(os.Stdout, true)
+		case "powershell":
+			cmd.Root().GenPowerShellCompletion(os.Stdout)
+		default:
+			cmd.Help()
+		}
+	},
+}
