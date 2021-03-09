bcbc25c98e2aae9df09b260000f1ba43d6526947
arout <arout@redhat.com>
2021-03-09 15:09:31 +0530

    Fixing CI failures

--- test/e2e/kamsuite/dirops.go
+++ test/e2e/kamsuite/dirops.go
@@ -3,13 +3,20 @@ package kamsuite
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
-// DirectoryShouldExist checks existing directory, throws error if not found
-func DirectoryShouldExist(dirName string) error {
-	if _, err := os.Stat(dirName); os.IsExist(err) {
+// BootstrapDirectoryShouldExist checks existing directory, throws error if not found
+func BootstrapDirectoryShouldExist(dirName string) error {
+	wd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	bootstrapDir := filepath.Join(wd, "out", "test-run", dirName)
+
+	if _, err := os.Stat(bootstrapDir); os.IsExist(err) {
 		return nil
 	}
 
-	return fmt.Errorf("directory %s exists", dirName)
+	return fmt.Errorf("No bootstrap %s directory exists", dirName)
 }
